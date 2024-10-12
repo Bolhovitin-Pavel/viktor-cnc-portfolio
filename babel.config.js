4eff4738be6b95fc66e29b2f93b810d421824eec
Bolhovitin-Pavel <bolhovitins.pavels@gmail.com>
2024-10-12 18:37:14 +0300

    react native web optimization fix

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   presets: ['next/babel'],
-  plugins: ['babel-plugin-react-native-web'],
+  plugins: [
+    // babel-plugin-react-native-web setup:
+    ['react-native-web', {commonjs: true}],
+  ],
 };
